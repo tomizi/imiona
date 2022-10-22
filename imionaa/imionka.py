5d566336524b016230b9155d7d47bbd9e9bf2907
tomizi <106354750+tomizi@users.noreply.github.com>
2022-10-22 15:56:49 +0200

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -31,4 +31,18 @@ if imiona is not None:
         st.write(e)
         
 
+if imiona is not None:
+    try:
+        im = pd.read_excel(imiona)
+        imie = st.text_input('Podaj imię: ','Martyna')
+        imie = imie.upper()
+        st.header('Wykres liniowy liczby dzieci o imieniu {} w zależnosci od czasu'.format(imie))
+        st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
+        st.balloons()
+    except Exception as e:
+        st.write('Czekam na dane')
+        st.write(e)
+        
+        
+
 
