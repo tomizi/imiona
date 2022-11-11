9f056b5f815e357f03348f6dc6977c90fefc13b4
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-11 19:42:25 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-st.set_page_config(page_title='Analiza imion nadanym dzieciom w latach 2000-2021', page_icon = ':family:', layout='wide')
+#st.set_page_config(page_title='Analiza imion nadanym dzieciom w latach 2000-2021', page_icon = ':family:', layout='wide')
 
 sekcja = st.sidebar.radio(
     'Wybierz sekcję:',
@@ -15,7 +15,7 @@ sekcja = st.sidebar.radio(
 im = pd.read_excel(io='imionaa/imiona.xlsx',engine='openpyxl',dtype={'Rok':str})
 
 if sekcja == 'Strona główna':
-    new_title = '<b style="color:rgb(0, 80, 170); font-size: 62px;">Strona główna</p>'
+    new_title = '<b style="color:rgb(0, 80, 170); font-size: 62px;">Analiza imion nadanym dzieciom w latach 2000-2021</p>'
     st.markdown(new_title, unsafe_allow_html=True)
     st.markdown('---')
     st.title('UNIWERSYTET MIKOIŁAJA KOPERNIKA W TORUNIU')
