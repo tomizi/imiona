a0cb62df199be07ac9dac65a85eafd65d425b341
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-11 20:33:27 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -44,6 +44,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     
     total_ur=im.pivot_table('Liczba', index='Rok', columns='Płeć', aggfunc=sum)	
     total_ur=pd.DataFrame(total_ur)
+    st.line_chart(total_ur)
 	
 
 
