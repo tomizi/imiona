de59bf954cb15ce549d827c4569694c0b12d0319
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-11 19:32:47 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -14,15 +14,17 @@ sekcja = st.sidebar.radio(
 
 im = pd.read_excel(io='imionaa/imiona.xlsx',engine='openpyxl',dtype={'Rok':str})
 
-st.title(':baby: Analiza imion nadanym dzieciom w latach 2000-2021')
-       
-imie = st.text_input('Podaj imię: ','Martyna')
-imie = imie.upper()
-st.header('Liczba dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
-st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
+if sekcja = 'Strona główna':
 
-st.header('Część dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
-st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600))
+    st.title(':baby: Analiza imion nadanym dzieciom w latach 2000-2021')
+       
+    imie = st.text_input('Podaj imię: ','Martyna')
+    imie = imie.upper()
+    st.header('Liczba dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
+    st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
+
+    st.header('Część dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
+    st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600))
 
 
 st.balloons()
