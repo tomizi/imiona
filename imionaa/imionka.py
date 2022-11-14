d923eba77ab88b5b32bb7ade308a3fdee9b052e4
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-14 14:19:55 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -71,9 +71,10 @@ if sekcja == 'Wyniki analizy statystycznej':
     	group=group.sort_values(by='Proporcja',ascending=False)
         return group.Proporcja.cumsum().values.searchsorted(q)+1
 
-    	diversity = top500.groupby(['Rok','Płeć']).apply(dla_kazdego_roku)
-    	diversity = diversity.unstack('Płeć')
-    	st.line_chart(diversity)
+    diversity = top500.groupby(['Rok','Płeć']).apply(dla_kazdego_roku)
+    diversity = diversity.unstack('Płeć')
+    divertisity=pd.DataFrame(divertisity, columns=['K','M'])
+    st.line_chart(diversity)
 
 
 st.balloons()
