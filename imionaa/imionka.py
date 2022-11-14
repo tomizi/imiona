e4ed0d4189b8016dd1b7c5de0717aa588b847c77
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-14 22:13:40 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -27,7 +27,7 @@ top500.reset_index(inplace=True, drop=True)
 
 #Ile imion wystarczy, żeby objąć 50% obserwacji (posortowane od najbardziej do najmnije popularnych)
 def dla_kazdego_roku(group,q=0.5):
-    	group=group.sort_values(by='Proporcja',ascending=False)
+    	group=group.sort_values(by='Proporcja%',ascending=False)
         return group.Proporcja.cumsum().values.searchsorted(q)+1
 
 
