1766e3aa2d2f978a613b0dc27c11d310074b56bb
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-14 10:03:30 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -30,7 +30,7 @@ if sekcja == 'Strona główna':
     #st.header('Liczba dzieci o nadanym imieniu {i} na przestrzeni lat 2000-2021'.format(i=str(imie)))
     st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
 
-    st.header('Część dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
+    #st.header('Część dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
     st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600))
 	
 
@@ -44,7 +44,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     #łączna ilość urodzeń
     total_ur=im.pivot_table('Liczba', index='Rok', columns='Płeć', aggfunc=sum)	
     total_ur=pd.DataFrame(total_ur, columns=['K','M'])
-    st.header('Łączna liczba dzieci urodzonych w latach 2000-2021 z podziałem na płeć')
+    #st.header('Łączna liczba dzieci urodzonych w latach 2000-2021 z podziałem na płeć')
     st.line_chart(total_ur)
  
     #top 100
