3b2549c33e9c9f7306e8e1863d78400a19f00c72
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-14 10:08:47 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -20,6 +20,7 @@ if sekcja == 'Strona główna':
     st.markdown(new_title, unsafe_allow_html=True)
     st.markdown('---')
     st.title('Strona główna')
+    st.header('COS')
     st.subheader('Wybór imienia dla dziecka to niełatwa decyzja. Powinna być świadomym wyborem obojga partnerów. ' +
 		 'Niektórzy rodzice kierują się modą, inni rodzinną tradycją, kolejni łatwością do zdrobnienia. ' +
 		 'Są też tacy, którzy zwracają uwagę na oryginalność lub po prostu brzmienie imienia.')
