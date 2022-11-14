3aecfe08709638ae6b810ad96fe55b8134bab178
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-14 22:23:42 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -77,7 +77,7 @@ if sekcja == 'Wyniki analizy statystycznej':
 	
     tabelka=top100.pivot_table('Proporcja',index='Rok',columns='Płeć',aggfunc=sum)
     tabelka=pd.DataFrame(tabelka, columns=['K','M'])
-    st.subheader('Część urodzonych dzieci, którym nadaje się imiona należące do listy 100 najpopularnijeszych imion')
+    st.subheader('Część urodzonych dzieci, którym nadaje się imiona należące do listy 100 najpopularniejszych imion')
     st.line_chart(tabelka)
 	
 	
