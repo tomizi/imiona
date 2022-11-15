1e9030469fda4320e23c42f76b847274a86cab9c
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-15 22:36:34 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -105,7 +105,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     litera_ulamek_k=pd.DataFrame(litera_ulamek_k)# columns=[2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021])
     rok = st.text_input('Podaj rok: ',2000)
     st.subheader('Liczba dzieci o nadanym imieniu {j} na przestrzeni lat 2000-2021'.format(j=str(rok)))
-    st.bar_chart(litera_ulamek_k[litera_ulamek_k['Rok']==rok])
+    st.bar_chart(litera_ulamek_k[litera_ulamek_k['Rok']==rok],x='Imię', y='Liczba')
     
 
     
