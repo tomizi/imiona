665cb2d6a5f0e297e2fedaf1e46fb11781c39326
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-28 11:26:07 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -108,7 +108,7 @@ if sekcja == 'Wyniki analizy statystycznej':
 		).update_layout(plot_bgcolor='white'))
     
     
-    st.dataframe(im['Imię'].sort_values(by='Imię').unique())
+    st.dataframe(im.sort_values(by='Imię')['Imię'].unique())
     
 
 
