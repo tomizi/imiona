7c899b2a9fdea0bb687ad86529c924cfaefeaca3
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-28 12:12:57 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -107,7 +107,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     st.plotly_chart(px.bar(litera_ulamek_k[str(rok)],y=str(rok)).update_xaxes(title_text='Pierwsza litera').update_yaxes(title_text='czesc'
 		).update_layout(plot_bgcolor='white'))
     
-    uni=pd.DataFrame({'litera':list(map(lambda x: x[0],im[im['Rok']==str(rok)].sort_values(by='Imię')['Imię'].unique()))}).groupby(['litera'])['litera'].count()
+    uni=pd.DataFrame({'litera':list(map(lambda x: x[0],im[im['Rok']==str(rok) & im['Płeć']=='K'].sort_values(by='Imię')['Imię'].unique()))}).groupby(['litera'])['litera'].count()
     st.plotly_chart(px.bar(uni,y='litera').update_xaxes(title_text='Litera').update_yaxes(title_text='Liczba imion'
 		).update_layout(plot_bgcolor='white'))
     
