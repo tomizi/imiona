6a91390234a370cb71021afe63fe7811e3569411
tomizi <106354750+tomizi@users.noreply.github.com>
2022-11-29 17:32:03 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -162,10 +162,9 @@ if sekcja == 'Wyniki analizy statystycznej':
     imie2 = st.text_input('Podaj drugie imię: ','Martyna')
     imie2 = imie2.upper()
     st.subheader('Porównanie trendu dla dwóch imion: {i} i {j}'.format(i=str(imie1), j=str(imie2)))
-    st.plotly_chart(px.line(im[im['Imię']==imie1],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
+    st.plotly_chart(px.line(im[im['Imię']==imie1,im[im['Imię']==imie2],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600))
 
-    st.subheader('Część dzieci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
-    st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600))
+    
     
 
 
