b78617cacb92d73adc8a5f3f713f69c05afbb229
tomizi <106354750+tomizi@users.noreply.github.com>
2022-12-05 11:07:52 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -202,8 +202,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(height=600,showlegend=False)
     col1, col2 = st.columns(2)
-    with col1:
-	st.write('ksakdk')
+  
    
  
 
