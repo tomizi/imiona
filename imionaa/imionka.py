f75076f29a6558828b66b8934bef4dcde6c2b2df
tomizi <106354750+tomizi@users.noreply.github.com>
2022-12-27 14:15:34 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -599,8 +599,11 @@ if sekcja == 'Analiza korespondencji':
     roczek=st.selectbox("Wybierz rok:", list(range(2019,2022)))
     st.title('Analiza korespondencji')
     if roczek==[2019]:
-	st.subheader('Imiona żeńskie')
-        st.image('imionaa/PI1/PI1/zk1.png')
+		st.image(imionaa/PI1/PI1/zk1.png)
+		
+		
+	#st.subheader('Imiona żeńskie')
+        #st.image('imionaa/PI1/PI1/zk1.png')
 	
    
     
