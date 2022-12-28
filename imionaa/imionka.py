233ac869b65d7b6c67ff0b91a879b80eb4003fbc
tomizi <106354750+tomizi@users.noreply.github.com>
2022-12-28 12:27:51 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -207,7 +207,7 @@ if sekcja == 'Wyniki analizy statystycznej':
     tab_dz=pd.DataFrame(tab_dz)
     st.dataframe(tab_dz)
     st.subheader('Top 10 imion żeńskich')
-    st.bar_chart(tab_dz['Rok'==rok])
+    st.bar_chart(tab_dz)['Rok'==rok]
 	
 	
 	
