b3504badd9c23614abdba2c0d3b114545cdd7a10
tomizi <106354750+tomizi@users.noreply.github.com>
2022-12-28 11:39:26 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -146,8 +146,8 @@ if sekcja == 'Wyniki analizy statystycznej':
 	
     # PIERWSZA LITERA
 	#liczba dziewczynek
-    st.header('Analiza pierwszej i ostatniej litery imienia')
     rok=st.selectbox("Wybierz rok:", list(range(2000,2022)))
+    st.header('Analiza pierwszej i ostatniej litery imienia')
     st.header('Pierwsza litera - imiona żeńskie')
     c1, c2 = st.columns(2)
     with c1:
