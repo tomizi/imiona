9636956d190506b40467760a200fe40ab3e5f806
tomizi <106354750+tomizi@users.noreply.github.com>
2022-12-28 13:56:54 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -601,14 +601,14 @@ if sekcja == 'Wyniki analizy statystycznej':
         col2.plotly_chart(px.bar(x=DF[DF['Województwo']==mies]['Liczba imion'][::-1],y=DF[DF['Województwo']==mies]['Imię'][::-1],
 			         orientation='h',text=DF[DF['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			         color_discrete_map="identity",
-			         title='Top 10 imon żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			         title='Top 10 imion żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				    ).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
      
         col1.plotly_chart(fig)
         col2.plotly_chart(px.bar(x=DF_c[DF_c['Województwo']==mies]['Liczba imion'][::-1],y=DF_c[DF_c['Województwo']==mies]['Imię'][::-1],
 			     orientation='h',text=DF_c[DF_c['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			     color_discrete_map="identity",
-			     title='Top 10 imon męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			     title='Top 10 imion męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
 	
     if wybrany == "2020":
@@ -617,14 +617,14 @@ if sekcja == 'Wyniki analizy statystycznej':
         col2.plotly_chart(px.bar(x=DF_2020_z[DF_2020_z['Województwo']==mies]['Liczba imion'][::-1],y=DF_2020_z[DF_2020_z['Województwo']==mies]['Imię'][::-1],
 			         orientation='h',text=DF_2020_z[DF_2020_z['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			         color_discrete_map="identity",
-			         title='Top 10 imon żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			         title='Top 10 imion żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				    ).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
         
         col1.plotly_chart(fig)
         col2.plotly_chart(px.bar(x=DF_2020_m[DF_2020_m['Województwo']==mies]['Liczba imion'][::-1],y=DF_2020_m[DF_2020_m['Województwo']==mies]['Imię'][::-1],
 			     orientation='h',text=DF_2020_m[DF_2020_m['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			     color_discrete_map="identity",
-			     title='Top 10 imon męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			     title='Top 10 imion męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
     if wybrany == "2019":
         
@@ -632,14 +632,14 @@ if sekcja == 'Wyniki analizy statystycznej':
         col2.plotly_chart(px.bar(x=DF_2019_z[DF_2019_z['Województwo']==mies]['Liczba imion'][::-1],y=DF_2019_z[DF_2019_z['Województwo']==mies]['Imię'][::-1],
 			         orientation='h',text=DF_2019_z[DF_2019_z['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			         color_discrete_map="identity",
-			         title='Top 10 imon żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			         title='Top 10 imion żeńskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				    ).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
       
         col1.plotly_chart(fig)
         col2.plotly_chart(px.bar(x=DF_2019_m[DF_2019_m['Województwo']==mies]['Liczba imion'][::-1],y=DF_2019_m[DF_2019_m['Województwo']==mies]['Imię'][::-1],
 			     orientation='h',text=DF_2019_m[DF_2019_m['Województwo']==mies]['Liczba imion'][::-1],color=["red"]*10,
 			     color_discrete_map="identity",
-			     title='Top 10 imon męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
+			     title='Top 10 imion męskich').update_xaxes(title_text='Liczba imion').update_yaxes(title_text='Imię'
 				).update_layout(plot_bgcolor='white',title_x=0.5,height=600))
 	
     
