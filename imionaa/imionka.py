a70c06a386212d416378f992254ec005f8c65fe5
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-24 08:39:58 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -78,8 +78,8 @@ if sekcja == 'Strona główna':
     imie = st.text_input('Podaj imię: ','Martyna')
     imie = imie.upper()
     st.subheader('Liczba dzieci o nadanym imieniu {i} na przestrzeni lat 2000-2021'.format(i=str(imie)))
-    st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600).update_yaxes(rangemode='tozero'))
-    st.dataframe(im[im['Imię']==imie])
+    st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Płeć','Rok']),x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600).update_yaxes(rangemode='tozero'))
+    st.dataframe(im[im['Imię']==imie].sort_values(['Płeć','Rok']))
 	
     st.subheader('Odsetek dzieci danej płci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
     st.plotly_chart(px.line(im[im['Imię']==imie],x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600).update_yaxes(rangemode='tozero'))
