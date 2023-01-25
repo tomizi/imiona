9a44046cd3835a21c7fd8eb78446742a22385e84
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-25 16:51:01 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -101,7 +101,7 @@ if sekcja == 'Analiza statystyczna':
     total_ur=im.pivot_table('Liczba', index='Rok', columns='Płeć', aggfunc=sum)	
     total_ur=pd.DataFrame(total_ur, columns=['K','M'])
     st.subheader('Łączna liczba dzieci urodzonych w latach 2000-2021 z podziałem na płeć')
-    st.line_chart(total_ur, x='Rok')
+    st.line_chart(total_ur, x_names='Liczba')
  
     #top 100
     def the_top100(group):
