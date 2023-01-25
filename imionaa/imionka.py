153bfc168b8808057b03a243b34aa359b33f057f
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-25 21:53:32 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -104,7 +104,7 @@ if sekcja == 'Strona główna':
     st.subheader('Liczba dzieci o nadanym imieniu {i} na przestrzeni lat 2000-2021'.format(i=str(imie)))
     st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],im[im['Imię']==imie]['Płeć'].unique()))).update_yaxes(rangemode='tozero').update_traces(line_width=2))
     st.dataframe(im[im['Imię']==imie].sort_values(['Rok','Płeć']))
-    st.write(im[im['Imię']==imie]['Płeć'].groupby('Płeć')['Rok'].astype(int).agg(np.min))
+    st.write(im[im['Imię']==imie]['Płeć'].groupby('Rok')['Płeć'].astype(int).agg(np.min))
 	#st.dataframe(im[im['Imię']==imie].sort_values(['Płeć','Rok']))
 	
     st.subheader('Odsetek dzieci danej płci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
