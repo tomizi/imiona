4d7854e2922c461470ef3243fd9dc91064053536
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-26 09:46:41 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -102,10 +102,7 @@ if sekcja == 'Strona główna':
     imie = st.text_input('Podaj imię:  ','Martyna')
     imie = imie.upper()
     st.subheader('Liczba dzieci o nadanym imieniu {i} na przestrzeni lat 2000-2021'.format(i=str(imie)))
-    if imie.empty:
-        st.write('*Brak danych dla wybranych imion')
-    else:
-        st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],im[im['Imię']==imie]['Płeć'].unique()))).update_yaxes(rangemode='tozero').update_traces(line_width=2))
+    st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],im[im['Imię']==imie]['Płeć'].unique()))).update_yaxes(rangemode='tozero').update_traces(line_width=2))
     
     
     #st.dataframe(im[im['Imię']==imie].sort_values(['Rok','Płeć']))
