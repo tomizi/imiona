4a5f67992daf9813d9bcc5a7a75e8e45d685b079
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-26 09:31:35 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -237,7 +237,7 @@ if sekcja == 'Analiza statystyczna':
 
 	#Najczęsciej nadawane imiona dzieciom w Polsce
     st.subheader("Imiona najczęściej nadawane dzieciom w Polsce w latach 2001-2021")
-    dowyboru=list(range(2000,2022))
+    dowyboru=list(range(2000,2022)[::-1])
     cos=st.selectbox("Wybierz rok:     ", dowyboru)
     def the_top10(group):
         return group.sort_values(by='Liczba', ascending=False)[:10]
