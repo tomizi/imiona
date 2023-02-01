85ecb988b89fa2d4e793f5ecf1be1c7b0fd93459
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-01 20:36:00 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -208,7 +208,7 @@ if sekcja == 'Analiza statystyczna':
     imionka2['Nowa'] = imionka2[['Imię','Płeć']].apply(lambda x: x['Imię']+str('(')+x['Płeć']+str(')'),axis=1)
     imionka2['Nowa2'] = imionka2[['Imię','Płeć']].apply(lambda x: x['Płeć']+str('(')+x['Imię']+str(')'),axis=1)
     #st.dataframe(imionka2)
-    st.dataframe(imionka2.sort_values(['Nowa','Rok','Nowa2']))
+    st.dataframe(imionka2.sort_values(['Rok','Nowa2']))
     koly = list(map(lambda x, y: x[0]+str(y),sorted(list(imionka2.sort_values(['Rok','Nowa2'])['Nowa2'].unique())),list(range(len(list(sorted(list(imionka2.sort_values(['Rok','Nowa2'])['Nowa2'].unique()))) ))) ))
     st.dataframe(koly)
     if imionka.empty:
