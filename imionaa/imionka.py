64f783804b8cc0d8c29ebe8dab3bd35e4438b340
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-01 14:12:46 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -114,7 +114,7 @@ if sekcja == 'Strona główna':
 	
     st.subheader('Procent dzieci danej płci o nadanym imieniu {} na przestrzeni lat 2000-2021'.format(imie))
     st.write('Wykres prezentuje liczbę dzieci o danym imieniu w ujęciu procentowym względem liczby zarejestrowanych dzieci, która mogła być różna w kolejnych latach. ')
-    st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],im[im['Imię']==imie]['Płeć'].unique()))).update_yaxes(title_text='Procent',rangemode='tozero'))
+    st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Proporcja%',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'].unique()))).update_yaxes(title_text='Procent',rangemode='tozero'))
     
 
 	
