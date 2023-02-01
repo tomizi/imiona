b73f9047b55568b1f9695cce3a8d1505d91d1cec
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-01 14:37:59 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -106,7 +106,7 @@ if sekcja == 'Strona główna':
     st.subheader('Liczba dzieci o nadanym imieniu {i} na przestrzeni lat 2000-2021'.format(i=str(imie)))
     st.dataframe(im[im['Imię']==imie].sort_values(['Rok','Płeć']))
     st.dataframe(im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'])
-    st.dataframe([im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'][index] for index in sorted(np.unique(im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'], return_index=True)[1])])))
+    st.dataframe([im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'][index] for index in sorted(np.unique(im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'], return_index=True)[1])])
     st.plotly_chart(px.line(im[im['Imię']==imie].sort_values(['Rok','Płeć']),x='Rok',y='Liczba',color='Płeć',markers=True,width=1100, height=600,color_discrete_sequence=list(map(lambda x: kol[x],[im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'][index] for index in sorted(np.unique(im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'], return_index=True)[1])]))).update_yaxes(rangemode='tozero').update_traces(line_width=2))
     st.dataframe(im[im['Imię']==imie].sort_values(['Rok','Płeć'])['Płeć'])
     
