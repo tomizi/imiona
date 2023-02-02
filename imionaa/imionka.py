2d4678a7bb9983a0ea225063e4e0bc0aa42cab4f
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 15:43:58 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -228,7 +228,7 @@ if sekcja == 'Analiza statystyczna':
     #if (np.min(list(im[im['Imię']==imie1]['Liczba']))>100)  and (np.min(list(im[im['Imię']==imie2]['Liczba']))>100) :
     #st.plotly_chart(px.line(imionka2.sort_values(['Rok','Nowa2']),x='Rok',y='Liczba',color='Nowa2',markers=True,width=1100, height=600,color_discrete_sequence=kol2[koly0]).update_xaxes().update_yaxes(rangemode='tozero'))
     if (imie1 in list(im['Imię']))  and (imie2 in list(im['Imię']) ):
-	    st.plotly_chart(px.bar(imionka3.sort_values(['Rok','Imię']),x=sorted(list(imionka3['Rok'])),y='Liczba',color='Imię',width=1100, height=600, barmode='group').update_xaxes(tickmode='array').update_yaxes(rangemode='tozero'))
+	    st.plotly_chart(px.line(imionka3.sort_values(['Rok','Imię']),x=sorted(list(imionka3['Rok'])),y='Liczba',color='Imię',width=1100, height=600, barmode='group').update_xaxes(tickmode='array').update_yaxes(rangemode='tozero'))
     else:
 	    st.write('*Brak danych dla wybranych imion')
     
