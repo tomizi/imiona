8544bfbf5186ed54d795fcf228add1a9a1a80a2a
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-02 14:30:30 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -221,7 +221,8 @@ if sekcja == 'Analiza statystyczna':
     st.dataframe(imionka2.sort_values(['Rok','Nowa2']))
     #st.write(koly0)
     #if (imie1 in list(im['Imię']))  and (imie2 in list(im['Imię']) )
-    if (np.min(list(im[im['Imię']==imie1]['Liczba']))>100)  and (np.min(list(im[im['Imię']==imie2]['Liczba']))>100) :
+    #if (np.min(list(im[im['Imię']==imie1]['Liczba']))>100)  and (np.min(list(im[im['Imię']==imie2]['Liczba']))>100) :
+    if (imie1 in list(im['Imię']))  and (imie2 in list(im['Imię']) ):
 	    st.plotly_chart(px.line(imionka2.sort_values(['Rok','Nowa2']),x='Rok',y='Liczba',color='Nowa2',markers=True,width=1100, height=600,color_discrete_sequence=kol2[koly0]).update_xaxes().update_yaxes(rangemode='tozero'))
     else:
 	    st.write('*Brak danych dla wybranych imion')
