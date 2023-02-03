024660dff8d0831552678250253feaa0148f00bd
tomizi <106354750+tomizi@users.noreply.github.com>
2023-02-03 20:09:21 +0100

    Update imionka.py

--- imionaa/imionka.py
+++ imionaa/imionka.py
@@ -236,7 +236,7 @@ if sekcja == 'Analiza statystyczna':
     #st.plotly_chart(px.line(imionka3.sort_values(['Rok','Imię']),x=sorted(list(imionka3['Rok'])),y='Liczba',color='Imię',width=1100, height=600, markers=True).update_xaxes(tickmode='array').update_yaxes(rangemode='tozero'))
     #st.plotly_chart(px.line(imionka2.sort_values(['Rok','Nowa2']),x='Rok',y='Liczba',color='Nowa2',markers=True,width=1100, height=600,color_discrete_sequence=kol2[koly0]).update_xaxes(dtick='M12').update_yaxes(rangemode='tozero'))
     if (imie1 in list(im['Imię']))  and (imie2 in list(im['Imię']) ):
-	    st.plotly_chart(px.line(imionka2.sort_values(['Rok','Nowa2']),x='Rok',y='Liczba',color='Nowa2',markers=True,width=1100, height=600,color_discrete_sequence=kol2[koly0]).update_xaxes(dtick='M12').update_yaxes(rangemode='tozero').update_layout(legend_title_text='Płeć oraz imię'),legend={'traceorder':'normal'})
+	    st.plotly_chart(px.line(imionka2.sort_values(['Rok','Nowa2']),x='Rok',y='Liczba',color='Nowa2',markers=True,width=1100, height=600,color_discrete_sequence=kol2[koly0]).update_xaxes(dtick='M12',range=['1999-06','2022-06']).update_yaxes(rangemode='tozero').update_layout(legend_title_text='Płeć oraz imię'),legend={'traceorder':'normal'})
     else:
 	    st.write('*Brak danych dla wybranych imion')
     
